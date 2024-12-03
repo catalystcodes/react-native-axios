9abadb1d8b204978c0b5ad41e20d47a35b47caab
catalystcodes <muh4mub@gmail.com>
2024-12-03 23:33:21 +0100

    feat: axious

--- App.js
+++ App.js
@@ -1,20 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import HomeScreen from "./src/screens/HomeScreen";
+import ProductScreen from "./src/screens/ProductScreen";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider style={{ flex: 1 }}>
+      <StatusBar />
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName="Home"
+          screenOptions={{ headerShown: false }}
+        >
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Product" component={ProductScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </SafeAreaProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/service/productService.js
@@ -0,0 +1,33 @@
+import axios from "axios";
+
+const API_URL = "https://fakestoreapi.com";
+
+const getProducts = async () => {
+  return new Promise((resolve, reject) => {
+    axios
+      .get(API_URL + "/products?limit=10")
+      .then((res) => {
+        // console.log(res.data);
+        resolve(res.data);
+      })
+      .catch((err) => {
+        reject(err);
+      });
+  });
+};
+
+const getProductById = async (id) => {
+  return new Promise((resolve, reject) => {
+    axios
+      .get(API_URL + "/products/" + id)
+      .then((res) => {
+        // console.log(res.data);
+        resolve(res.data);
+      })
+      .catch((err) => {
+        reject(err);
+      });
+  });
+};
+
+export { getProducts, getProductById };
