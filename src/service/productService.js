d7aa273b0eee68d7abbda7e9423a9d36fb043a42
catalystcodes <muh4mub@gmail.com>
2024-12-04 12:15:59 +0100

    chore: clean up

--- src/service/productService.js
+++ src/service/productService.js
@@ -31,3 +31,67 @@ const getProductById = async (id) => {
 };
 
 export { getProducts, getProductById };
+
+// const baseUrl = "https://reqres.in";
+
+// // Passing configuration object to axios
+// axios({
+//   method: "get",
+//   url: `${baseUrl}/api/users/1`,
+// }).then((response) => {
+//   // console.log(response.data);
+// });
+
+// // Invoking the get method to perform a GET request
+
+// axios.get(`${baseUrl}/api/users/1`).then((response) => {
+//   // console.log(response.data);
+// });
+
+// //async await method
+// const fetchUsers = async () => {
+//   const configurationObject = {
+//     method: "get",
+//     url: `${baseUrl}/api/users/1`,
+//   };
+//   const response = await axios(configurationObject);
+//   // console.log(response.data);
+// };
+
+// // Invoking get method to perform a GET request
+// const fetchUser = async () => {
+//   const url = `${baseUrl}/api/users/1`;
+//   const response = await axios.get(url);
+//   // console.log(response.data);
+// };
+
+// const concurrentRequests = [
+//   axios.get(`${baseUrl}/api/users/1`),
+//   axios.get(`${baseUrl}/api/users/2`),
+//   axios.get(`${baseUrl}/api/users/3`),
+// ];
+
+// promises
+//   .all(concurrentRequests)
+//   .then((result) => {
+//     console.log(result);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+
+// //using promise .allSettled() method to handle errors for concurrent requests
+// const concurrentRequests1 = [
+//   axios.get(`${baseUrl}/api/users/1`),
+//   axios.get(`${baseUrl}/api/users/2`),
+//   axios.get(`${baseUrl}/api/users/3`),
+// ];
+
+// promises
+//   .allSettled(concurrentRequests1)
+//   .then((result) => {
+//     console.log(result);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
